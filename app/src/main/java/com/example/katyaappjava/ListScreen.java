f7ff691c1fcb1abb6708d3e8e2db55152b2ddeec
YHNRH <kdog2105@mail.ru>
2021-04-26 21:46:32 +0300

    Added string constants

--- app/src/main/java/com/example/katyaappjava/ListScreen.java
+++ app/src/main/java/com/example/katyaappjava/ListScreen.java
@@ -3,12 +3,53 @@ package com.example.katyaappjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListScreen extends AppCompatActivity {
+
+    TextView tw1;
+    TextView tw2;
+    TextView tw3;
+    TextView tw4;
+    TextView tw5;
+    TextView tw6;
+    TextView tw7;
+    TextView tw8;
+    TextView tw9;
+    TextView tw10;
+    TextView tw11;
+    TextView tw12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_screen);
+        tw1 = findViewById(R.id.p1);
+        tw2= findViewById(R.id.p2);
+        tw3 = findViewById(R.id.p3);
+        tw4 = findViewById(R.id.p4);
+        tw5 = findViewById(R.id.p5);
+        tw6 = findViewById(R.id.p6);
+        tw7 = findViewById(R.id.p7);
+        tw8 = findViewById(R.id.p8);
+        tw9 = findViewById(R.id.p9);
+        tw10 = findViewById(R.id.p10);
+        tw11 = findViewById(R.id.p11);
+        tw12 = findViewById(R.id.p12);
+        tw1.setText(R.string.nobj1);
+        tw2.setText(R.string.nobj2);
+        tw3.setText(R.string.nobj3);
+        tw4.setText(R.string.nobj4);
+        tw5.setText(R.string.nobj5);
+        tw6.setText(R.string.nobj6);
+        tw7.setText(R.string.nobj7);
+        tw8.setText(R.string.nobj8);
+        tw9.setText(R.string.nobj9);
+        tw10.setText(R.string.nobj10);
+        tw11.setText(R.string.nobj11);
+        tw12.setText(R.string.nobj12);
+
     }
 }
--- app/src/main/java/com/example/katyaappjava/PointScreen.java
+++ app/src/main/java/com/example/katyaappjava/PointScreen.java
@@ -3,12 +3,107 @@ package com.example.katyaappjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PointScreen extends AppCompatActivity {
-
+    private int obj =1;
+    TextView tw;
+    ImageView iw;
+    TextView next;
+    TextView prev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_point_screen);
+        tw = findViewById(R.id.text);
+        iw = findViewById(R.id.img);
+        next = findViewById(R.id.next);
+        prev = findViewById(R.id.previous);
+        setTextAndImg();
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(obj!=12) {
+                    obj++;
+                    setTextAndImg();
+                }
+            }
+        });
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(obj!=1) {
+                    obj--;
+                    setTextAndImg();
+                }
+            }
+        });
+
+    }
+    private void setTextAndImg(){
+        switch (obj){
+            case 1:
+                tw.setText(R.string.obj1);
+                iw.setImageResource(R.drawable.obj1);
+                break;
+
+            case 2:
+                tw.setText(R.string.obj2);
+                //  iw.setImageResource(R.drawable.obj2);
+                break;
+
+            case 3:
+                tw.setText(R.string.obj3);
+                //     iw.setImageResource(R.drawable.obj3);
+                break;
+
+            case 4:
+                tw.setText(R.string.obj4);
+                //     iw.setImageResource(R.drawable.obj4);
+                break;
+
+            case 5:
+                tw.setText(R.string.obj5);
+                //     iw.setImageResource(R.drawable.obj5);
+                break;
+
+            case 6:
+                tw.setText(R.string.obj6);
+                //      iw.setImageResource(R.drawable.obj6);
+                break;
+
+            case 7:
+                tw.setText(R.string.obj7);
+                //    iw.setImageResource(R.drawable.obj7);
+                break;
+
+            case 8:
+                tw.setText(R.string.obj8);
+                //     iw.setImageResource(R.drawable.obj8);
+                break;
+
+            case 9:
+                tw.setText(R.string.obj9);
+                //     iw.setImageResource(R.drawable.obj9);
+                break;
+
+            case 10:
+                tw.setText(R.string.obj10);
+                //     iw.setImageResource(R.drawable.obj10);
+                break;
+
+            case 11:
+                tw.setText(R.string.obj11);
+                //     iw.setImageResource(R.drawable.obj11);
+                break;
+            case 12:
+                tw.setText(R.string.obj12);
+                //      iw.setImageResource(R.drawable.obj12);
+                break;
+        }
+
+
     }
 }
