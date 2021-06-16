03d775ace1352eaa7a25ea423bd99283b724eca4
YHNRH <kdog2105@mail.ru>
2021-06-16 04:35:21 -0400

    Release alpha

--- app/src/main/java/com/example/katyaappjava/MapsActivity.java
+++ app/src/main/java/com/example/katyaappjava/MapsActivity.java
@@ -2,23 +2,28 @@ package com.example.katyaappjava;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private int obj = 1;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        obj = getIntent().getIntExtra("point", 1);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -39,44 +44,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng point1 = new LatLng(55.66604 , 43.58825);
+        LatLng point1 = new LatLng(55.66558 ,43.59014);
         mMap.addMarker(new MarkerOptions().position(point1).title(getResources().getString(R.string.nobj1)));
 
-        LatLng point2 = new LatLng(55.66604 , 43.58825);
+        LatLng point2 = new LatLng(55.66604 ,43.58825);
         mMap.addMarker(new MarkerOptions().position(point2).title(getResources().getString(R.string.nobj2)));
 
-        LatLng point3 = new LatLng(55.66604 , 43.58825);
+        LatLng point3 = new LatLng(55.66831 ,43.58379);
         mMap.addMarker(new MarkerOptions().position(point3).title(getResources().getString(R.string.nobj3)));
 
-        LatLng point4 = new LatLng(55.66604 , 43.58825);
+        LatLng point4 = new LatLng(55.67097 ,43.57933);
         mMap.addMarker(new MarkerOptions().position(point4).title(getResources().getString(R.string.nobj4)));
 
-        LatLng point5 = new LatLng(55.66604 , 43.58825);
+        LatLng point5 = new LatLng(55.67419 ,43.57744);
         mMap.addMarker(new MarkerOptions().position(point5).title(getResources().getString(R.string.nobj5)));
 
-        LatLng point6 = new LatLng(55.66604 , 43.58825);
+        LatLng point6 = new LatLng(55.67608 ,43.57512);
         mMap.addMarker(new MarkerOptions().position(point6).title(getResources().getString(R.string.nobj6)));
 
-        LatLng point7 = new LatLng(55.66604 , 43.58825);
+        LatLng point7 = new LatLng(55.67477 ,43.58096);
         mMap.addMarker(new MarkerOptions().position(point7).title(getResources().getString(R.string.nobj7)));
 
-        LatLng point8 = new LatLng(55.66604 , 43.58825);
+        LatLng point8 = new LatLng(55.67661 ,43.59211);
         mMap.addMarker(new MarkerOptions().position(point8).title(getResources().getString(R.string.nobj8)));
 
-        LatLng point9 = new LatLng(55.66604 , 43.58825);
+        LatLng point9 = new LatLng(55.67514 ,43.59130);
         mMap.addMarker(new MarkerOptions().position(point9).title(getResources().getString(R.string.nobj9)));
 
-        LatLng point10 = new LatLng(55.66604 , 43.58825);
+        LatLng point10 = new LatLng(55.67337 ,43.59362);
         mMap.addMarker(new MarkerOptions().position(point10).title(getResources().getString(R.string.nobj10)));
 
-        LatLng point11 = new LatLng(55.66604 , 43.58825);
+        LatLng point11 = new LatLng(55.67158 ,43.58881);
         mMap.addMarker(new MarkerOptions().position(point11).title(getResources().getString(R.string.nobj11)));
-
-        LatLng point12 = new LatLng(55.66604 , 43.58825);
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                String id = marker.getId().substring(1);
+                Intent intent = new Intent(getApplicationContext(), PointScreen.class);
+                intent.putExtra("point", Integer.parseInt(id)+1);
+                startActivity(intent);
+                return false;
+            }});
+        LatLng point12 = new LatLng(55.66729 ,43.59623);
         mMap.addMarker(new MarkerOptions().position(point12).title(getResources().getString(R.string.nobj12)));
-
         mMap.moveCamera(CameraUpdateFactory.zoomTo( 16.0f ));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(point1));
+        switch (obj) {
+            case(1):
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(point1));
+            break;
+            case(2):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point2));
+                break;
+            case(3):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point3));
+                break;
+            case(4):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point4));
+                break;
+            case(5):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point5));
+                break;
+            case(6):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point6));
+                break;
+            case(7):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point7));
+                break;
+            case(8):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point8));
+                break;
+            case(9):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point9));
+                break;
+            case(10):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point10));
+                break;
+            case(11):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point11));
+                break;
+            case(12):
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(point12));
+                break;
+        }
 //        mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
     }
 }
--- app/src/main/java/com/example/katyaappjava/PointScreen.java
+++ app/src/main/java/com/example/katyaappjava/PointScreen.java
@@ -2,6 +2,8 @@ package com.example.katyaappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,17 +14,21 @@ public class PointScreen extends AppCompatActivity {
     TextView tw;
     ImageView iw;
     TextView next;
+    TextView toMap;
     TextView prev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_point_screen);
         obj = getIntent().getIntExtra("point", 0);
+
         tw = findViewById(R.id.text);
+        toMap = findViewById(R.id.toMap1);
         iw = findViewById(R.id.img);
         next = findViewById(R.id.next);
         prev = findViewById(R.id.previous);
         setTextAndImg();
+
         next.setOnClickListener(v -> {
             if(obj!=12) {
                 switch (obj){
@@ -91,89 +97,114 @@ public class PointScreen extends AppCompatActivity {
         });
 
     }
+    public void toMap(View v){
+        if (obj!=0&&obj!=101&&obj!=301&&obj!=801&&obj!=1101) {
+            Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+            intent.putExtra("point", obj);
+            startActivity(intent);
+        }
+
+    }
     private void setTextAndImg(){
         switch (obj){
             case 0:
+                toMap.setBackgroundColor(Color.GRAY);
                 tw.setText(R.string.obj0);
                 iw.setImageResource(R.drawable.obj0);
                 break;
             case 101:
+                toMap.setBackgroundColor(Color.GRAY);
                 tw.setText(R.string.obj1_1);
                 iw.setImageResource(R.drawable.obj1_1);
                 break;
 
             case 301:
+                toMap.setBackgroundColor(Color.GRAY);
                 tw.setText(R.string.obj3_1);
                 iw.setImageResource(R.drawable.obj3_1);
                 break;
 
             case 801:
+                toMap.setBackgroundColor(Color.GRAY);
                 tw.setText(R.string.obj8_1);
                 iw.setImageResource(R.drawable.obj8_1);
                 break;
 
             case 1101:
+                toMap.setBackgroundColor(Color.GRAY);
                 tw.setText(R.string.obj11_1);
                 iw.setImageResource(R.drawable.obj11_1);
                 break;
 
             case 1:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj1);
                 iw.setImageResource(R.drawable.obj1);
                 break;
 
             case 2:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj2);
-                //  iw.setImageResource(R.drawable.obj2);
+                  iw.setImageResource(R.drawable.obj2);
                 break;
 
             case 3:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj3);
-                //     iw.setImageResource(R.drawable.obj3);
+                     iw.setImageResource(R.drawable.obj3);
                 break;
 
             case 4:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj4);
-                //     iw.setImageResource(R.drawable.obj4);
+                     iw.setImageResource(R.drawable.obj4);
                 break;
 
             case 5:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj5);
-                //     iw.setImageResource(R.drawable.obj5);
+                     iw.setImageResource(R.drawable.obj5);
                 break;
 
             case 6:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj6);
-                //      iw.setImageResource(R.drawable.obj6);
+                      iw.setImageResource(R.drawable.obj6);
                 break;
 
             case 7:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj7);
-                //    iw.setImageResource(R.drawable.obj7);
+                    iw.setImageResource(R.drawable.obj7);
                 break;
 
             case 8:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj8);
-                //     iw.setImageResource(R.drawable.obj8);
+                     iw.setImageResource(R.drawable.obj8);
                 break;
 
             case 9:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj9);
-                //     iw.setImageResource(R.drawable.obj9);
+                     iw.setImageResource(R.drawable.obj9);
                 break;
 
             case 10:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj10);
-                //     iw.setImageResource(R.drawable.obj10);
+                     iw.setImageResource(R.drawable.obj10);
                 break;
 
             case 11:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj11);
-                //     iw.setImageResource(R.drawable.obj11);
+                     iw.setImageResource(R.drawable.obj11);
                 break;
             case 12:
+                toMap.setBackgroundResource(R.color.green);
                 tw.setText(R.string.obj12);
-                //      iw.setImageResource(R.drawable.obj12);
+                      iw.setImageResource(R.drawable.obj12);
                 break;
         }
 
